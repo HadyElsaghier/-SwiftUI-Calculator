f845d99ac45145b9ec880eaa2c93e841e86f82f8
HadyElsaghier <HadyElsaghier@github.com>
2022-09-01 18:02:02 +0200

    Initial commit

---
+++ -SwiftUI-Calculator/_SwiftUI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  _SwiftUI_CalculatorApp.swift
+//  -SwiftUI-Calculator
+//
+//  Created by Hady Elsaghier on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct _SwiftUI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
